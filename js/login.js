5820f7983f9dd797a9aa261a8240e6f98bdb456f
Md Abdul Quaiyum Chowdhury <Md.Abdul.Quaiyum.Chowdhury@bodo.kommune.no>
2025-04-26 12:27:20 +0200

    last commit

--- js/login.js
+++ js/login.js
@@ -1,17 +1,35 @@
-/* step 1 - set event handler */
-document.querySelector('button').addEventListener('click', function(event){
+// /* step 1 - set event handler */
+// document.querySelector('button').addEventListener('click', function(event){
 
-    // step 2 - prevent default behaviuor (prevent reloading browser)
-    event.preventDefault() /* it means page will not reload. and if we work with the form we have to add extra line */
+//     // step 2 - prevent default behaviuor (prevent reloading browser)
+//     event.preventDefault() /* it means page will not reload. and if we work with the form we have to add extra line */
 
-    /* step -3 get the phone number */
-    const phoneNumber = document.getElementById('phone-number')
-    const newValue = phoneNumber.value
-    console.log(newValue);
+//     /* step -3 get the phone number and pin number */
+//     const phoneNumber = document.getElementById('phone-number')
+//     const newValue = phoneNumber.value
+//     const pinNumber = document.getElementById('pin-number').value
+//     console.log(pinNumber, newValue);
+    
+//     // step 4 is to validate phone number and pin number the phone number and the pin number. but we should not do this. this is just temporary method
 
-    /* step -4 to get he pin number */
-    const pinNumber = document.getElementById('pin-number');
-    const insertedPin= pinNumber.value
-    console.log(insertedPin);
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('you are logged in')
+//         /* step 5 is to allow user to use the website */
+//     }else{
+//         alert('wrong phonenumber or pin')
+//     }
 
+// })
+
+document.querySelector('button').addEventListener('click', function(event){ /* i am using here event parameter as i will take data from user. as it is form so i must need to write parameter */
+  
+    event.preventDefault() /* - it must need to use to stop relaoding as form ususally get reloaded */
+ const phoneNumber = document.getElementById('phone-number').value;
+ const pinNumber = document.getElementById('pin-number').value
+ if(phoneNumber === '5' && pinNumber === '1234'){
+    console.log('you logged in');
+    window.location.href = './home.html' /* through this one i have taken it to the home page  */
+ }else{
+    alert('phonenumber or pinumber is error')
+ }
 })
