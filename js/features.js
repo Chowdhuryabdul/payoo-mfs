ed049710211cd841d012be44c1fc1c5473479135
Md Abdul Quaiyum Chowdhury <Md.Abdul.Quaiyum.Chowdhury@bodo.kommune.no>
2025-05-20 14:51:15 +0200

    siste

--- js/features.js
+++ js/features.js
@@ -9,6 +9,7 @@ document.getElementById('btn-show-cashout').addEventListener('click', function()
     document.getElementById('add-money-form').classList.add('hidden')
 })
 
+// show the add money form and hide the cash out form 
 
 document.getElementById('btn-show-add-money').addEventListener('click', function(){
     document.getElementById('cash-out-form').classList.add('hidden')
